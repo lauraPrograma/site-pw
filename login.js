80330047e5b3772d9e200cfaf4e1177cb08e3a71
Thaís <thaisdutra2009@gmail.com>
2022-08-11 20:32:59 -0300

    código alterado

--- login.js
+++ login.js
@@ -1,13 +1,13 @@
-const pass_field = document.querySelector(".pass-key");
+const senha = document.querySelector(".senha");
 const showBtn = document.querySelector(".show");
 
 showBtn.addEventListener("click", function () {
-    if(pass_field.type === "password") {
-        pass_field.type = "text";
+    if(senha.type === "password") {
+        senha.type = "text";
         showBtn.textContent = "ocultar";
         showBtn.style.color = "#3498db"
     }else {
-        pass_field.type = "password";
+        senha.type = "password";
         showBtn.textContent = "mostrar";
         showBtn.style.color = "#222";
     }
