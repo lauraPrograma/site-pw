eabde44058bbffcb2138c334da6df7d79b9d93cf
Thaís <thaisdutra2009@gmail.com>
2022-09-03 23:57:33 -0300

    alteracao

--- login.js
+++ login.js
@@ -1,24 +1,11 @@
-const senha = document.querySelector(".senha");
-const showBtn = document.querySelector(".show");
-const loginBtn = document.querySelector("button");
-const user = document.querySelector(".user")
-
-showBtn.addEventListener("click", function () {
-    if(senha.type === "password") {
-        senha.type = "text";
-        showBtn.textContent = "ocultar";
-        showBtn.style.color = "#3498db"
-    }else {
-        senha.type = "password";
-        showBtn.textContent = "mostrar";
-        showBtn.style.color = "#222";
-    }
-});
 
 
-loginBtn.addEventListener("click", function () {
-    if(user.value == 'Thais' || user.value == 'Sofia' || user.value == 'Laura' || user.value == 'Pablo') {
+button.addEventListener('click', () => {
+    if ((user.value == 'Thais' && pass == '1234') || (user == 'Laura' && pass == '1234')) {
         window.location.href = "file:///C:/Users/User/Documents/site-pw/inicial.html";
         alert(`${user.value} , seu login foi efetuado com sucesso!`);
     }
-})
+    else {
+        document.querySelector('#message').innerHTML = 'Usuário ou senha inválidos';
+    }
+});
